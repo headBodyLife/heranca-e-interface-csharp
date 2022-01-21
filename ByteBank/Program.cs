5b262d6bb8ea00f3b59dc073a87f40d41904365e
headBodyLife <allan.bt167@gmail.com>
2022-01-21 16:25:41 -0300

    Classe Funcionario

---
+++ ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ByteBank.Funcionarios;
+namespace ByteBank
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario(1);
+            funcionario1.Nome = "Carlos";
+            funcionario1.CPF = "312.454.677-22";
+            funcionario1.Salario = 4800;
+
+            Console.WriteLine(funcionario1.Nome);
+            Console.WriteLine(funcionario1.GetBonificacao());
+
+
+        }
+    }
+}
