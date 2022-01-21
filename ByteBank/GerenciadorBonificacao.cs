d0d5d71e0f42e948bac1f979feb8d259d2ddc2a6
headBodyLife <allan.bt167@gmail.com>
2022-01-21 19:07:49 -0300

    Classe Diretor e Sobrecargas

---
+++ ByteBank/GerenciadorBonificacao.cs
@@ -0,0 +1,25 @@
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonificacao += diretor.GetBonificacao();
+        }
+
+        public double GetTotalBeneficacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
